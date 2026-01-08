887b1f55bac14dc4c15dad492c1119ed8c159989
陳彥翔 <chenyanxiang@chenyanxiangdeMacBook-Air.local>
2026-01-09 01:36:16 +0900

    feat(admin): add CustomNav component with icons and grouping

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -45,6 +45,8 @@ export default buildConfig({
       // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: ['@/components/BeforeDashboard#BeforeDashboard'],
+      // Custom Navigation
+      Nav: '@/components/Admin/CustomNav#CustomNav',
     },
     user: Users.slug,
     dateFormat: 'yyyy-MM-dd HH:mm',
